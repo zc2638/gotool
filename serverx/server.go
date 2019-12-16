56c77463625e114a9e6d79c94146f4654d77aabf
zc <zc2638@qq.com>
2019-12-16 11:52:14 +0800

    add graceful exit web server

---
+++ serverx/server.go
@@ -0,0 +1,54 @@
+package serverx
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
+	"time"
+)
+
+/**
+ * Created by zc on 2019/12/16.
+ */
+// 平滑退出服务
+func GracefulExitWeb(server *http.Server) {
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
+	sig := <-ch
+
+	fmt.Println("got a signal", sig)
+
+	now := time.Now()
+	cxt, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(cxt)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	// 看看实际退出所耗费的时间
+	fmt.Println("延时", time.Since(now), "退出")
+}
+
+// CPU性能查看
+func CpuProf(fc func()) {
+
+	f, err := os.OpenFile("cpu.prof", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	err = pprof.StartCPUProfile(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer pprof.StopCPUProfile()
+	fc()
+}
